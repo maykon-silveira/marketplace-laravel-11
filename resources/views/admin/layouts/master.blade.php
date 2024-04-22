990d7b8a9c080fa161474f8eec89ce9431007e2b
unknown <contato@webtecpr.com.br>
2024-04-22 07:52:42 -0300

    first commit

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Painel Admisnistrativo  &mdash; MaykonSilveira.com.br</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/fontawesome/css/all.min.css') }}">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/summernote/summernote-bs4.css') }}">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/components.css') }}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+      <!-- START NAVABAR - MAYKONSILVEIRA.COM.BR -->
+       @include('admin.layouts.navbar')
+      <!-- END NAVBAR - MAYKONSILVEIRA.COM.BR -->
+
+      <!-- START SIDEBAR - MAYKONSILVEIRA.COM.BR -->
+      @include('admin.layouts.sidebar')
+      <!-- END SIDEBAR - MAYKONSILVEIRA.COM.BR -->
+
+      <!-- START MAIN CONTENT - MAYKONSILVEIRA.COM.BR -->
+      <div class="main-content">
+
+       @yield('content')
+
+      </div>
+      <!-- END MAIN CONTENT - MAYKONSILVEIRA.COM.BR -->
+
+      <!-- START FOOTER - MAYKONSILVEIRA.COM.BR -->
+      <footer class="main-footer">
+        <div class="footer-left">
+          Todos os Direitos Reservados &copy; <?= date('Y') ?> <div class="bullet"></div> Desenvolvido <a href="https://maykonsilveira.com.br/">MaykonSilveira.com.br</a> Versão 1.0
+        </div>
+        <div class="footer-right">
+
+        </div>
+      </footer>
+      <!-- END FOOTER - MAYKONSILVEIRA.COM.BR -->
+    </div>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="{{ asset('backend/assets/modules/jquery.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/popper.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/tooltip.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/moment.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/stisla.js') }}"></script>
+
+  <!-- JS Libraies -->
+  <script src="{{ asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/chart.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/summernote/summernote-bs4.js') }}"></script>
+  <script src="{{ asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
+  <!-- Page Specific JS File -->
+  <script src="{{ asset('backend/assets/js/page/index-0.js') }}"></script>
+
+  <!-- Template JS File -->
+  <script src="{{ asset('backend/assets/js/scripts.js') }}"></script>
+  <script src="{{ asset('backend/assets/js/custom.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html">MSFLIX</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">St</a>
+      </div>
+      <ul class="sidebar-menu">
+        <li class="menu-header">Painel</li>
+        <li class="dropdown active">
+          <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+          <ul class="dropdown-menu">
+            <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+            <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+          </ul>
+        </li>
+        <li class="menu-header">Starter</li>
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+            <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+            <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+          </ul>
+        </li>
+        <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+
+    
+    </ul>
+
+    </aside>
+  </div>
